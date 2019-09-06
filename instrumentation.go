98b5e38a927c037701be72d82ac54c1f98396bc1
Praveen Shukla <praveen.shukla.c42@gmail.com>
2019-09-06 21:52:05 +0530

    Add status code and content length as label in http_requests_total metrics

--- instrumentation.go
+++ instrumentation.go
@@ -2,6 +2,7 @@ package red
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -13,7 +14,7 @@ var HTTPRequests = promauto.NewCounterVec(
 		Name: "http_requests_total",
 		Help: "How many HTTP requests processed, partitioned by status code and HTTP method.",
 	},
-	[]string{"status_code", "method", "route"},
+	[]string{"status_code", "method", "route", "content_length"},
 )
 
 // Register - Registers the Prometheus metrics
@@ -21,9 +22,30 @@ func Register() {
 	prometheus.Register(HTTPRequests)
 }
 
+type statusWriter struct {
+	http.ResponseWriter
+	Status int
+	Length int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.Status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *statusWriter) Write(b []byte) (int, error) {
+	if w.Status == 0 {
+		w.Status = 200
+	}
+	n, err := w.ResponseWriter.Write(b)
+	w.Length += n
+	return n, err
+}
+
 func InstrumentationMiddleware(n http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		n.ServeHTTP(w, r)
-		HTTPRequests.WithLabelValues("200", r.Method, r.RequestURI).Inc()
+		sw := statusWriter{ResponseWriter: w}
+		n.ServeHTTP(&sw, r)
+		HTTPRequests.WithLabelValues(strconv.Itoa(sw.Status), r.Method, r.RequestURI, strconv.Itoa(sw.Length)).Inc()
 	})
 }
